90cae393183ea24810740695d73e00c1c6401155
Edward Chen <edward-chen@Edwards-MacBook-Pro.local>
2022-11-05 19:38:55 +1100

    test

--- app.py
+++ app.py
@@ -9,8 +9,12 @@ app = Flask(__name__)
 config = r"--psm 11 --oem 3"
 
 UPLOAD_FOLDER = 'static/uploads/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/')
@@ -18,9 +22,8 @@ def home():
     arr = os.listdir(UPLOAD_FOLDER)
     print(arr)
     for file in arr:
-        os.remove(os.path.join(UPLOAD_FOLDER, file))
+        os.remove(os.path.join(app.config['UPLOAD_FOLDER'], file))
     return render_template('index.html')
-
 
 @app.route('/', methods=['POST'])
 def upload_image():
@@ -33,34 +36,28 @@ def upload_image():
         return redirect(request.url)
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        save_path = os.path.join(UPLOAD_FOLDER, filename)
+        save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename);
         file.save(save_path)
         img = cv2.imread(save_path)
-        text = pytesseract.image_to_string(img, config=config)
+        text = pytesseract.image_to_string(img,config=config);
         flash('Image successfully uploaded and text displayed below')
-        data = pytesseract.image_to_data(img, config=config, output_type=Output.DICT)
+        data = pytesseract.image_to_data(img,config=config,output_type=Output.DICT);
         amount_boxes = len(data['text'])
 
         for i in range(amount_boxes):
             if float(data['conf'][i]) > 50:
-                (x, y, width, height) = (data['left'][i], data['top'][i], data['width'][i], data['height'][i])
-                img = cv2.rectangle(img, (x, y), (x + width, y + height), (0, 255, 0), 2)
-        cv2.imwrite(save_path, img)
+                (x,y,width,height) = (data['left'][i],data['top'][i],data['width'][i],data['height'][i])
+                img = cv2.rectangle(img,(x,y),(x+width,y+height),(0,255,0),2)
+        cv2.imwrite(save_path,img);
 
         return render_template('index.html', filename=filename, text=text)
     else:
         flash('Allowed image types are - png, jpg, jpeg')
         return redirect(request.url)
 
-
 @app.route('/display/<filename>')
 def display_image(filename):
     return redirect(url_for('static', filename='uploads/' + filename), code=301)
-
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
